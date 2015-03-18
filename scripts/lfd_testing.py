Modified Radial Basis Function approximation to have the correct center and variance values to approximate the demonstration appropriately. I have tested this in one dimension for both the RBF and the linear function approximators in following sine waves. However, there is a noticeble phase shift and slight increased amplitude in the reproduced movement when given the same tau, starting state, and goal state. This might have something to do with the initialization of the numerical integration when doing the lfd, but I'm not sure yet.

scripts/lfd_testing.py
@@ -61,27 +61,35 @@ def makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh,
 if __name__ == '__main__':
     rospy.init_node('dmp_tutorial_node')
 
-    #Create a DMP from a 2-D trajectory
-    dims = 2                
-    dt = 1.0                
+    #Create a DMP from a 1-D trajectory
+    dims = 1                
+    dt = 0.1                
     K = 100                 
     D = 2.0 * np.sqrt(K)      
     num_bases = 100          
-    traj = [[1.0,1.0],[2.0,2.0],[3.0,4.0],[6.0,8.0]]
+    w = np.pi/2;
+    num_dem_pts = 100;
+    traj = [[ np.cos(w*dt*j) for i in range(dims) ]for j in range(num_dem_pts) ]
     resp = makeLFDRequest(dims, traj, dt, K, D, num_bases)
 
     #Set it as the active DMP
     makeSetActiveRequest(resp.dmp_list)
 
     #Now, generate a plan
-    x_0 = [0.0,0.0]          #Plan starting at a different point than demo 
-    x_dot_0 = [0.0,0.0]   
+    #x_0 = [1.0]          #Plan starting at a different point than demo 
+    #x_dot_0 = [0.0]   
+    x_0 = [traj[0][0]] 
+    x_dot_0 = [0.0]
     t_0 = 0                
-    goal = [8.0,7.0]         #Plan to a different goal than demo
-    goal_thresh = [0.2,0.2]
+    #goal = [0.0]         #Plan to a different goal than demo
+    #goal = [0.95105];
+    #goal = [-2.0]
+    goal = [traj[num_dem_pts-1][0]]
+    #print traj[num_dem_pts-1][0]
+    goal_thresh = [0.2]
     seg_length = -1          #Plan until convergence to goal
-    tau = 2 * resp.tau       #Desired plan should take twice as long as demo
-    dt = 1.0
+    tau = 1 * resp.tau       #Desired plan should take twice as long as demo
+    dt = 0.1
     integrate_iter = 5       #dt is rather large, so this is > 1  
     plan = makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
                            seg_length, tau, dt, integrate_iter)
@@ -90,26 +98,25 @@ if __name__ == '__main__':
     num_pts  = len(plan.plan.points)+1
     positions= [ [ 0 for i in range(num_pts) ] for j in range(dims) ]
     positions[0][0] = x_0[0]
-    positions[1][0] = x_0[1]
     #print plan
     for i in xrange(1,num_pts):
         positions[0][i] = plan.plan.points[i-1].positions[0]
-        positions[1][i] = plan.plan.points[i-1].positions[1]
 
     times = plan.plan.times
     times = (0,)+times
 
-    num_dem_pts = len(traj)
-    traj_arr = [ [ 0 for i in range(num_dem_pts) ] for j in range(dims) ]
+    traj_arr = [[ 0 for i in range(num_dem_pts) ]for j in range(dims) ]
     for i in xrange(0,num_dem_pts):
         traj_arr[0][i] = traj[i][0]
-        traj_arr[1][i] = traj[i][1]
 
-    print positions
-    print plan
+    #print positions
+    #print plan
 
+    # plt.plot(times[0:num_dem_pts-90],positions[0][0:num_dem_pts-90],'r')
     plt.plot(times,positions[0],'r')
     #plt.plot(times,positions[1])
-    plt.plot(times[0:num_dem_pts],traj_arr[0],'g')
+    demo_time = [ i*dt for i in range(num_dem_pts) ]
+    plt.plot(demo_time,traj_arr[0],'g')
+    #plt.plot(demo_time,resp.dmp_list[0].f_targets,'b')
     plt.ylabel('new_plan')
     plt.show()